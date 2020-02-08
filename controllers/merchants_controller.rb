2f955d04a22a63bc973589c008caaf384b409b11
danyibalazs <danyi.balage@gmail.com>
2020-02-08 18:37:19 +0000

    add new merchant option created

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -7,3 +7,13 @@ get '/merchants' do
   @merchants = Merchant.all()
   erb ( :"merchants/index" )
 end
+
+get '/merchants/new' do
+  erb ( :"merchants/new" )
+end
+
+post '/merchants' do
+  merchant = Merchant.new(params)
+  merchant.save()
+  redirect to("/merchants")
+end
