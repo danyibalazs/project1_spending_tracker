0039300dc29b955ce1f31a3a4fca2e72bd7e93b2
danyibalazs <danyi.balage@gmail.com>
2020-02-08 14:34:59 +0000

    add crud methods to transaction

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require("pry-byebug")
 require_relative("../models/merchant")
 require_relative("../models/tag")
+require_relative("../models/transaction")
 
+Transaction.delete_all()
 Merchant.delete_all()
 Tag.delete_all()
 
@@ -24,6 +26,20 @@ tag2 = Tag.new({
   "name" => "entertainment"
   })
 tag2.save()
+
+transaction1 = Transaction.new({
+  "merchant_id" => merchant1.id,
+  "tag_id" => tag1.id,
+  "amount" => 100
+  })
+transaction1.save()
+
+transaction2 = Transaction.new({
+  "merchant_id" => merchant2.id,
+  "tag_id" => tag2.id,
+  "amount" => 50
+  })
+transaction2.save()
 
 binding.pry
 nil
---
+++ models/transaction.rb
@@ -0,0 +1,58 @@
+require_relative( '../db/sql_runner' )
+
+class Transaction
+
+  attr_reader :id
+  attr_accessor :merchant_id, :tag_id, :amount
+
+  def initialize(options)
+    @id = options["id"].to_i() if options["id"]
+    @merchant_id = options["merchant_id"].to_i()
+    @tag_id = options["tag_id"].to_i()
+    @amount = options["amount"].to_f()
+  end
+
+  def save()
+    sql = "INSERT INTO transactions
+    (merchant_id, tag_id, amount) VALUES ($1, $2, $3)
+    RETURNING id"
+    value = [@merchant_id, @tag_id, @amount]
+    result = SqlRunner.run(sql, value)
+    @id = result[0]["id"].to_i()
+  end
+
+  def update()
+    sql = "UPDATE transactions
+    SET (merchant_id, tag_id, amount) = ($1, $2, $3)
+    WHERE id = $4"
+    values = [@merchant_id, @tag_id, @amount, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM transactions WHERE id = $1"
+    value = [@id]
+    SqlRunner.run(sql, value)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM transactions"
+    results = SqlRunner.run(sql)
+    transactions = results.map {|transaction| Transaction.new(transaction)}
+    return transactions
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM transactions WHERE id = $1"
+    value = [id]
+    result = SqlRunner.run(sql, value)
+    transaction = Tag.new(result[0])
+    return transaction
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM transactions"
+    SqlRunner.run(sql)
+  end
+
+end
