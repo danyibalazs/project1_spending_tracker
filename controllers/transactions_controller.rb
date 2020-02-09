0037ba62281d676f340d600045fc09b4955e8085
danyibalazs <danyi.balage@gmail.com>
2020-02-09 12:23:53 +0000

    add new transaction option

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -1,6 +1,8 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/transaction' )
+require_relative( '../models/merchant' )
+require_relative( '../models/tag' )
 also_reload( '../models/*' )
 
 get '/transactions' do
@@ -8,4 +10,16 @@ get '/transactions' do
   @transactions.reverse!
   @total = Transaction.total_amounts(@transactions)
   erb ( :"transactions/index" )
+end
+
+get '/transactions/new' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb ( :"transactions/new" )
+end
+
+post '/transactions' do
+  transaction = Transaction.new(params)
+  transaction.save
+  redirect to("/transactions")
 end
