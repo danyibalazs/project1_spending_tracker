dfd1aaf3895f976dd7faeb9b987652b0539595a3
danyibalazs <danyi.balage@gmail.com>
2020-02-09 12:06:59 +0000

    add edit/delete to tags

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -17,3 +17,25 @@ post '/tags' do
   merchant.save()
   redirect to("/tags")
 end
+
+get '/tags/:id' do
+  @tag = Tag.find(params["id"].to_i)
+  erb (:"tags/show")
+end
+
+post '/tags/:id/delete' do
+  tag = Tag.find(params["id"].to_i)
+  tag.delete()
+  redirect("/tags")
+end
+
+get '/tags/:id/edit' do
+  @tag = Tag.find(params["id"].to_i)
+  erb (:"tags/edit")
+end
+
+post '/tags/:id' do
+  tag = Tag.new(params)
+  tag.update()
+  redirect("/tags")
+end
